b0162b27869f2c3d2e4aa70bf392b35a73129bbc
EDHC-JohnJohnson <162633121+EDHC-JohnJohnson@users.noreply.github.com>
2024-03-08 16:09:33 -0600

    Update StartURL.ps1

--- StartURL.ps1
+++ StartURL.ps1
@@ -1,1 +1,2 @@
--StartOSDCloud "-OSName 'Windows 11 23H2 x64' -OSLanguage en-us -OSEdition Pro -OSActivation Retail"
+#-StartOSDCloud "-OSName 'Windows 11 23H2 x64' -OSLanguage en-us -OSEdition Pro -OSActivation Retail"
+Start-OSDCloud -OSLanguage en-us -OSBuild 23H2 -OSEdition Enterprise -OSActivation Retail
