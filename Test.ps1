18192fa4557a2d1b57cd274c4a8b01fec844dcd4
EDHC-JohnJohnson <john.johnson@edhc.com>
2024-03-12 12:09:03 -0500

    Delete Test.ps1

--- Test.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host -ForegroundColor Cyan "... Starting EDHC custom OSD Cloud ..."
-Clear-Host
-
-Import-Module OSD -Force
-Install-Module OSD -Force
-
-Start-OSDCloud -OSLanguage en-us -OSBuild 23H2 -OSEdition Enterprise -ZTI
-
-wpeuril reboot
