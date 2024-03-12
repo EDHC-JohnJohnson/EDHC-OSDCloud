571e7e437b4fd35af1a6cb020a7a84ac719f8578
EDHC-JohnJohnson <162633121+EDHC-JohnJohnson@users.noreply.github.com>
2024-03-12 10:57:25 -0500

    Update StartURL.ps1

--- StartURL.ps1
+++ StartURL.ps1
@@ -1,2 +1,25 @@
-#-StartOSDCloud "-OSName 'Windows 11 23H2 x64' -OSLanguage en-us -OSEdition Pro -OSActivation Retail"
-Start-OSDCloud -OSLanguage en-us -OSBuild 23H2 -OSEdition Enterprise -OSActivation Retail
+Write-Host  -ForegroundColor Cyan "Starting EDHC Custom OSDCloud ..."
+cls
+Write-Host "======================== Menu =========================" -ForegroundColor Blue
+Write-Host "=======================================================" -ForegroundColor Blue
+Write-Host "1: Manual CLI"-ForegroundColor Cyan
+Write-Host "2: Manual GUI"-ForegroundColor Cyan
+Write-Host "3: Zero-Touch Win11 23H2 | English | Enterprise" -ForegroundColor Cyan
+Write-Host "4: Exit"-ForegroundColor Cyan
+
+$input = Read-Host "Please make a selection"
+
+Write-Host  -ForegroundColor Blue "Loading OSDCloud..."
+
+Import-Module OSD -Force
+Install-Module OSD -Force
+
+switch ($input)
+{
+    '1' { Start-OSDCloudGUI } 
+    '2' { Start-OSDCloud } 
+    '3' { Start-OSDCloud -OSLanguage en-us -OSBuild 23H2 -OSEdition Enterprise -ZTI } 
+    '4' { Exit }
+}
+
+wpeutil reboot
