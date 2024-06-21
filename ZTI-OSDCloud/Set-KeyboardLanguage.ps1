cff811cc131eae2379d2e564aae3c01af23a6e33
EDHC-JohnJohnson <john.johnson@edhc.com>
2024-06-21 11:31:29 -0500

    Update ZTI

---
+++ ZTI-OSDCloud/Set-KeyboardLanguage.ps1
@@ -0,0 +1,19 @@
+#Source: https://raw.githubusercontent.com/AkosBakos/OSDCloud/main/Set-KeyboardLanguage.ps1
+
+$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Set-KeyboardLanguage.log"
+Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
+
+Write-Host -ForegroundColor Green "Setting Keyboard language to en-US"
+Start-Sleep -Seconds 5
+
+$LanguageList = Get-WinUserLanguageList
+
+Set-WinUserLanguageList $LanguageList -Force
+
+Start-Sleep -Seconds 5
+
+$LanguageList = Get-WinUserLanguageList
+$LanguageList.Remove(($LanguageList | Where-Object LanguageTag -like 'en-US'))
+Set-WinUserLanguageList $LanguageList -Force
+
+Stop-Transcript
