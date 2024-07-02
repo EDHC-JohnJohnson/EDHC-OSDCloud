69f659afaa5f8ea83efbc8da75f86e2608125650
John Johnson <162633121+EDHC-JohnJohnson@users.noreply.github.com>
2024-07-02 13:16:14 -0500

    Update Start-ZTI.ps1
    Add formatting and setup complete section in PostOS

--- Start-ZTI.ps1
+++ Start-ZTI.ps1
@@ -1,3 +1,44 @@
-Start-OSDCloud -OSLanguage en-us -OSBuild 23H2 -OSEdition Pro -OSLicense Retail -ZTI
+#================================================
+#   [PreOS] Update Module
+#================================================
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
 
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force   
+
+#=======================================================================
+#   [OS] Params and Start-OSDCloud
+#=======================================================================
+$Params = @{
+    OSVersion = "Windows 11"
+    OSBuild = "23H2"
+    OSEdition = "Pro"
+    OSLanguage = "en-us"
+    OSLicense = "Retail"
+    ZTI = $true
+    Firmware = $true
+}
+Start-OSDCloud @Params
+
+#================================================
+#  [PostOS] SetupComplete CMD Command Line
+#================================================
+Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
+$SetupCompleteCMD = @'
+powershell.exe -Command Set-ExecutionPolicy Bypass -Session -Force
+powershell.exe -Command Write-Output "THIS IS A TEST"
+'@
+$SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
+
+#=======================================================================
+#   Restart-Computer
+#=======================================================================
+Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
 wpeutil reboot
